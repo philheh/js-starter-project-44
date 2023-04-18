311901fbbda94dde33dce0deb8b36c95d04cf7ce
philheh <zuev.phillip@gmail.com>
2023-04-19 01:16:41 +0300

    nothing add

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import welcome, { welcomeMsg, userName } from '../src/cli.js';
+import { welcomeMsg, userName } from '../src/cli.js';
 import evenMechanic from '../src/brain-even-mechanic.js';
 
 welcomeMsg(userName);
