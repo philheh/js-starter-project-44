e343264d165ad324e94d71fbd51bcbadbf7662e8
philheh <zuev.phillip@gmail.com>
2023-04-18 19:49:19 +0300

    step5 refactoring brain-even mechanic

--- src/brain-even-mechanic.js
+++ src/brain-even-mechanic.js
@@ -6,27 +6,15 @@ const evenMechanic = (userName) => {
   while (rounds < 3) {
     const randomInt = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${randomInt}`);
-    const correctAnswer = randomInt % 2 === 0;
+    const correctAnswer = randomInt % 2 === 0 ? 'yes' : 'no';
     const answer = readlineSync.prompt();
-    if (correctAnswer && answer === 'yes') {
-      rounds += 1;
-      console.log('Correct!');
-    } else if (!correctAnswer && answer === 'no') {
-      rounds += 1;
-      console.log('Correct!');
-    } else if (answer === 'yes' && !correctAnswer) {
+    if ((answer !== 'yes' && answer !== 'no') || answer !== correctAnswer) {
       return console.log(
-        "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!",
-      );
-    } else if (answer === 'no' && correctAnswer) {
-      return console.log(
-        "'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!",
-      );
-    } else {
-      return console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer ? 'yes' : 'no'}'.\nLet's try again, Bill!`,
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`
       );
     }
+    console.log('Correct!');
+    rounds += 1;
   }
   return console.log(`Congratulations, ${userName}!`);
 };
